25d11af861698f8273b27ac8057282e5a86015e3
tehran980 <hsn.m@torkmail.ir>
2016-01-07 02:18:20 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,28 +220,19 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+    sudo_users = {93367845,71979053,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v1
+    about_text = [[tele_HSN v1
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/tehran980/tele_HSN2
 
 Admins
-@iwals [Founder]
 @imandaneshi [Developer]
-@seyedan25 [Manager]
+@FloWeD [Developer]
 
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
 ]],
     help_text = [[
 Commands list :
